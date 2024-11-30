d0a732aeac1cd9b1d2b3fa87a693b4a2ee9aaa41
mtahagenc <mtahagenc@gmail.com>
2024-11-30 11:02:30 +0300

    Initial Commit

---
+++ FeyzaGold/FeyzaGoldApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeyzaGoldApp.swift
+//  FeyzaGold
+//
+//  Created by FeyzaGold on 30.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct FeyzaGoldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FeyzaGoldTests/FeyzaGoldTests.swift
@@ -0,0 +1,16 @@
+//
+//  FeyzaGoldTests.swift
+//  FeyzaGoldTests
+//
+//  Created by FeyzaGold on 30.11.2024.
+//
+
+import Testing
+
+struct FeyzaGoldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
